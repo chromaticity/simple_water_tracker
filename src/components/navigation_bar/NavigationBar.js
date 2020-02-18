8eb0031277b2bca87b0457fe996abbf7fe71bf08
Brandon <brandonantil457@gmail.com>
2020-02-18 11:28:22 -0800

    Added in a template for navigation component

---
+++ src/components/navigation_bar/NavigationBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const navigationBar = (props) => {
+    return(
+        <div>
+            <h1>Navigation Bar</h1>
+        </div>
+    );
+}
+
+export default navigationBar;
